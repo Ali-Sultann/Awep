1ad2efcf938a3727581be40e069de691d31ba73a
Ali-Sultann <ali.sultann8@gmail.com>
2025-09-10 21:54:27 -0400

    Implements all required functions

--- smallapps/giphy-party/index.js
+++ smallapps/giphy-party/index.js
@@ -1,3 +1,46 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
-const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const giphyApiKeyWeb = "KN57tpiNfx7T083oGmqfSyEFo8T1vjgh";
+const gifUrl = "https://api.giphy.com/v1/gifs/";
+
+async function giphyRequest(query, limit) {
+    const response = await axios.get(
+        `${gifUrl}search?api_key=${giphyApiKeyWeb}&q=${query}&limit=5&limit=${limit}`
+    );
+    return response.data;
+}
+
+const gifForm = document.getElementById("gifSearchForm");
+const gifContainer = document.getElementById("gifContainer");
+const gifQuery = document.getElementById("gifQuery");
+const clearBtn = document.getElementById("clearBtn");
+console.log("hello");
+
+gifForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    // gifContainer.innerHTML = "Loading...";
+    if (gifContainer.childElementCount === 0) {
+        gifContainer.innerHTML = "";
+    }
+
+    const query = gifQuery.value;
+    try {
+        const data = await giphyRequest(query, 1);
+        console.log(data);
+        console.log(data.data[0].images);
+
+        data.data.forEach((gif) => {
+            const img = document.createElement("img");
+            img.src = gif.images.fixed_width.url;
+            img.alr = gif.title;
+            gifContainer.appendChild(img);
+        });
+    } catch (e) {
+        console.log(e);
+        gifContainer.innerHTML = `${e.message}`;
+    }
+});
+
+clearBtn.addEventListener("click", (e) => {
+    gifContainer.innerHTML = "";
+});
